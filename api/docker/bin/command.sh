730bee2cd906dc3ba178099a052b667ad2d18ff3
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2024-02-21 17:54:54 +0900

    feat: fastapi で DB と接続できるように変更 | 初期テーブル作成・初期データ挿入用スクリプトを作成

--- api/docker/bin/command.sh
+++ api/docker/bin/command.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
+# .env ファイルが無ければ .env.example からコピーする
+if [ ! -e "/usr/src/app/.env" ]; then
+    cp /usr/src/app/.env.example /usr/src/app/.env
+fi
+
 # パッケージのインストール
 poetry install --no-root
+
+# DB が起動するまで待機
+while true; do
+    curl db:3306
+
+    if [ $? -eq 1 ]; then
+        break
+    fi
+
+    sleep 1
+done
+
+# マイグレーション
+poetry run python -m database.migrate
+
+# 初期データの投入
+poetry run python -m database.seed
 
 # サーバの起動
 poetry run uvicorn main:app --reload --host 0.0.0.0
