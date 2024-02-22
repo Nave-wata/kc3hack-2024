b1964bcde01833a9d02cd8a413be7d226ec783ba
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2024-02-23 06:16:33 +0900

    feat: ボケボットのデータをバックエンドから取得し、簡単なボケボットコンポーネントを作成

---
+++ app/src/features/bokes/api/getBokes.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { axios } from "../../../lib/axios";
+
+export function getBokes() {
+  return axios.get("/boke");
+}
+
+export function useBokes() {
+  return useQuery({
+    queryKey: ["quizzes"],
+    queryFn: () => getBokes(),
+  });
+}
