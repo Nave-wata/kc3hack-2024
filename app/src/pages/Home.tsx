3e82b30ebd300308c18498f99d760ff584391d6d
nakatani33 <134926425+nakatani33@users.noreply.github.com>
2024-02-22 21:40:07 +0900

    タイトルにルール説明を追加した

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -5,7 +5,9 @@ import { GameComponent } from "../components/Game";
 import Background from '../assets/images/Background/background.png';
 import {Box,
     Button,
-    Grid
+    Grid,
+    Modal,
+    Typography
 }from "@mui/material";
 
 const head = () => {
@@ -29,9 +31,26 @@ const image = {
     backgroundPosition: 'center'
 };
 
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    borderRadius: '16px',
+};
+
+const closebutton = {
+    textAlign: "right",
+};
+
 export const Home = () => {
     const [isFullScreen, setIsFullScreen] = React.useState<boolean>(document.fullscreenElement ? true : false);
-
+    const [open, setOpen] = React.useState(false);
     const handleGameStart = () => {
         if (document.fullscreenElement) {
             document.exitFullscreen();
@@ -42,19 +61,54 @@ export const Home = () => {
         }
     }
 
+    const handleOpen = () => setOpen(true);
+
+    const handleClose = () => setOpen(false);
+
     const HomeComponent = () => (
         <Box sx={image}>
-            <Box sx={{ minHeight: '100vh', minWidth: '100vw', justifyContent: "space-between" }}>
-                <Grid container spacing={2} direction="column" alignItems="center" justifyContent="center" style={{minHeight: '100vh' }} >
-                    <Grid item xs={12} sx={{ paddingBottom: 3 }}>
-                        <Box sx={{ marginTop: '300px'}}>
-                        <Button variant="contained" color='inherit' size="large" onClick={() => handleGameStart()}>
-                            ゲームスタート
-                        </Button>
+              <Box sx={{ minHeight: '100vh', minWidth: '100vw', justifyContent: "space-between" }}>
+                <Grid container spacing={1} alignItems="center" justifyContent="center" direction="column">
+                    <Grid item xs={12} sx={{ paddingBottom: 4}}>
+                        <Box sx={{marginTop:'380px'}}>
+                            <Button variant="contained" onClick={() => handleGameStart()}>ゲームスタート</Button>
                         </Box>
                     </Grid>
+                    <Grid item xs={12} sx={{ paddingBottom: 4 }}>
+                        <Button variant="contained" color='inherit' onClick={handleOpen}>ルール</Button>
+                    </Grid>
                 </Grid>
+                
             </Box>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" align="center" variant="h4" component="h2">
+                        ⚁ルール⚂
+                    </Typography>
+                    <Typography id="modal-modal-description" align="center" sx={{ mt: 2 }}>
+                        <b>関西の観光地をすごろくでまわろう‼‼</b><br />
+                    </Typography>
+                    <Typography id="modal-modal-description" >
+                        <br />
+                        ・対戦人数は1～4人<br />
+                        ・先頭のプレイヤーがゴールに到達するとゲーム終了<br />
+                        ・イベントマスに止まるとミニゲームスタート！<br />
+                        ・ミニゲームで勝利するとポイントゲット！<br />
+                        ・勝敗はポイントが一番多い人が勝利<br />
+                        <br />
+
+                        <strong>ミニゲームでたくさん勝利してポイントを稼ごう‼‼‼<br /></strong>
+                    </Typography>
+                    <Box sx={closebutton}>
+                        <Button variant="contained" onClick={handleClose}>閉じる</Button>
+                    </Box>
+                </Box>
+            </Modal>
         </Box>
     );
 
