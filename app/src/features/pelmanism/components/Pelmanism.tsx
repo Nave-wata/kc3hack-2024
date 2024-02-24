1fe4e34062ea877ece48e9ea91879820b7453d1b
morishintarou <134926335+morishintarou@users.noreply.github.com>
2024-02-24 21:15:34 +0900

    すごろくに戻るボタンの実装

--- app/src/features/pelmanism/components/Pelmanism.tsx
+++ app/src/features/pelmanism/components/Pelmanism.tsx
@@ -5,7 +5,11 @@ import { PelmanismItem, PelmanismResponse } from "../types";
 import { getRandomValues, shuffleArray } from "../../../utils/array";
 import CardImage from "../../../assets/images/Card/cardback.png";
 import "../index.css";
-import { Box, Button, Grid } from "@mui/material";
+import {
+  Box, Button, Grid, Modal,
+  Typography
+} from "@mui/material";
+import { GameComponent } from "../../../components/Game";
 
 export function Pelmanism({ pairNumber }: { pairNumber: number }) {
   const [pelmanismItems, setPelmanismItems] = React.useState<PelmanismItem[]>([]);
@@ -13,6 +17,26 @@ export function Pelmanism({ pairNumber }: { pairNumber: number }) {
   const [isShows, setIsShows] = React.useState<boolean[]>([]);
   const [isResetting, setIsResetting] = React.useState<boolean>(false);
   const pelmanismQuery = usePelmanism();
+  const [open, setOpen] = React.useState(false);
+  const [isGameFinished, setGameFinished] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
+
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    borderRadius: '16px',
+  };
+
 
   React.useEffect(() => {
     if (is_set<PelmanismResponse[]>(pelmanismQuery.data)) {
@@ -53,6 +77,11 @@ export function Pelmanism({ pairNumber }: { pairNumber: number }) {
     setOpenItems([]);
   };
 
+  const returnGame = () => {
+
+    return <div><GameComponent /></div>
+  }
+
   if (pelmanismQuery.isLoading) {
     return <div>Loading...</div>;
   }
@@ -63,6 +92,7 @@ export function Pelmanism({ pairNumber }: { pairNumber: number }) {
 
   return (
     <Box style={{ backgroundColor: "purple", minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+      {isGameFinished && returnGame()}
       <Grid container >
         <Grid item xs={8}>
           <Box style={{ display: "flex", flexWrap: "wrap", width: "800px", height: "400px", marginBottom: "20rem" }}>
@@ -92,6 +122,7 @@ export function Pelmanism({ pairNumber }: { pairNumber: number }) {
         <Grid container item xs={3}>
           <Grid item xs={12}>
             <Grid container direction="column" alignItems="center" marginLeft="-1.5rem">
+              <Grid item xs={12} sx={{ display: "flex", textAlign: "center", height: "100%" }}><br /><br /></Grid>
               <Grid item xs={12} sx={{ display: "flex", textAlign: "center", height: "100%" }}>スコア</Grid>
               <Grid item xs={12}>
                 <Box sx={{ display: "flex", textAlign: "center", height: "100%" }}>1</Box>
@@ -99,16 +130,43 @@ export function Pelmanism({ pairNumber }: { pairNumber: number }) {
             </Grid>
           </Grid>
           <Grid item xs={12}>
-            <Button>ルール説明</Button>
+            <Button variant="contained" onClick={handleOpen}>ルール説明</Button>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <Typography id="modal-modal-title" align="center" variant="h4" component="h2">
+                  ⚁ルール⚂
+                </Typography>
+                <Typography id="modal-modal-description" align="center" sx={{ mt: 2 }}>
+                  <b>関西の観光地をすごろくでまわろう‼‼</b><br />
+                </Typography>
+                <Typography id="modal-modal-description" >
+                  <br />
+                  ・対戦人数は1～4人<br />
+                  ・先頭のプレイヤーがゴールに到達するとゲーム終了<br />
+                  ・イベントマスに止まるとミニゲームスタート！<br />
+                  ・ミニゲームで勝利するとポイントゲット！<br />
+                  ・勝敗はポイントが一番多い人が勝利<br />
+                  <br />
+
+                  <strong>ミニゲームでたくさん勝利してポイントを稼ごう‼‼‼<br /></strong>
+                </Typography>
+                <Box sx={{ textAlign: "right", }}>
+                  <Button variant="contained" onClick={handleClose}>閉じる</Button>
+                </Box>
+              </Box>
+            </Modal>
           </Grid>
           <Grid item xs={12}>
-            <Box sx={{ display: "flex", textAlign: "center", height: "100%" }}>スコア</Box>
-          </Grid>
-          <Grid item xs={12}>
-            <Box sx={{ display: "flex", textAlign: "center", height: "100%" }}>スコア</Box>
+            <Button onClick={() => setGameFinished(true)}>すごろくに戻る</Button>
           </Grid>
         </Grid>
       </Grid>
     </Box >
+
   );
 }
